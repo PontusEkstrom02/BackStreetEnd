63def4a6343f58a096d3669a8323273b24eb4067
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-04-12 13:18:44 +0200

    socket stuff

--- index.js
+++ index.js
@@ -24,9 +24,16 @@ app.post("/broadcast/:msg", (request, response) => {
 */
 // 1. [GET] - http://address:port/ducks/api/channel/ <-- hämtar en lista över annonserade kanaler. Se VG kritierier för krav till ett högre betyg.
 app.get("/ducks/api/channel/", (request, response) => {
-  SocketService.broadcast("chats", request.params.msg)
+  const channels = SocketService.getAllChannels();
 
-  response.sendStatus(200);
+  response.status(200).json(channels);
+})
+//[GET] - http://adress:port/ducks/api/channel/:id <-- 
+//hämtar innehållet i en identiferad kanal som tidigare har annonserats ut, detta syftar på meddelanden som har skickats i kanalen.
+app.get("/ducks/api/channel/:id", (request, response) => {
+  const channels = SocketService.getAllChannels();
+
+  response.status(200).json(channels);
 })
 
 app.post("/send/:username", (request, response) => {
@@ -37,8 +44,6 @@ app.post("/send/:username", (request, response) => {
 
   response.sendStatus(200);
 });
-
-
 
 
 
--- service/socketService.js
+++ service/socketService.js
@@ -7,17 +7,15 @@ const options = {
   }
 }
 
-
-
 let io = undefined;
 let clients = [];
-let Channels = [];
-/*
-function getAllChannels(){
-  Channels = 
+let Channels = {};
+
+function getAllChannels() {
+  return Object.keys(Channels);
 }
-*/
-function handleNewConnection(clientSocket) {
+
+function handleNewConnection(clientSocket, channel) {
   // 1. Sparar client sockets för ex. broadcasts via rest anrop
   clients.push(clientSocket);
 
@@ -26,31 +24,46 @@ function handleNewConnection(clientSocket) {
   // 2. Plockar bort klienten från listan när anslutningen för klienten avbryts
   clientSocket.on("disconnect", () => {
     clients = clients.filter(client => client != clientSocket);
+
+    // Remove the client from the corresponding channel
+    if (Channels[channel]) {
+      Channels[channel] = Channels[channel].filter(client => client !== clientSocket);
+    }
+  });
+
+  // Add the client to the corresponding channel
+  if (Channels[channel]) {
+    Channels[channel].push(clientSocket);
+  } else {
+    Channels[channel] = [clientSocket];
+  }
+}
+
+// Function to broadcast a message to all clients in a channel
+function broadcast(channel, message) {
+  if (Channels[channel]) {
+    Channels[channel].forEach(client => client.emit(channel, message));
+  }
+}
+
+// Function to send a private message to a specific user in a channel
+function sendToUser(username, channel, message) {
+  if (Channels[channel]) {
+    const matchedClients = Channels[channel].filter(client => client.username === username);
+    matchedClients.forEach(client => client.emit(channel, message));
+  }
+}
+
+// Function to attach socket.io to a container with a specific channel
+function attach(container, channel) {
+  io = new Server(container, options);
+  io.of(channel).on("connection", (clientSocket) => {
+    handleNewConnection(clientSocket, channel);
   });
 }
 
-function sendToUser(username, message) {
-  const matchedClients = clients.filter(client => client.username == username);
-  matchedClients.forEach(client => client.emit("private", message));
-  
-  
-//const client = clients.find(client => client.username == username);
-//client.emit("private", message); // skickar endast till EN klient
-}
-
-function sendChatsToUser(username){
-  // fix change username to key
-  const client = clients.find(client => client.username == username);
-  client.emit("private", message); // skickar endast till EN klient
-}
-
-function broadcast(channel, message) {
-  io.emit(channel, message); //broadcastar till alla på socket servern
-}
-
-function attach(container) {
-  io = new Server(container, options);
-  io.on("connection", handleNewConnection);
-}
+const channel = 'myChannel';
+const message = 'Hello everyone!';
+broadcast(channel, message);
 
 export default { broadcast, attach, sendToUser }
