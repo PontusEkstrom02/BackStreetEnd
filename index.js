54c01b6dee134ad19de798ed6e349d897f0127f4
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-04-13 21:16:59 +0200

    trying frontend

--- index.js
+++ index.js
@@ -101,8 +101,43 @@ app.delete("/ducks/api/channel/:id", (req, res) => {
   }
 });
 
-/*[GET] - http://adress:port/ducks/api/broadcast/
+/*6.[GET] - http://adress:port/ducks/api/broadcast/
 hämtar en lista över alla händelser som har skickats ut, ex. älgvandring, traffikolycker m.m.*/
+app.get("/ducks/api/broadcast/", (req, res) => {
+  let channelId = "broadcast";
+
+  // Check if the channel exists
+  if (SocketService.channels[channelId]) {
+    // Return the messages in the channel
+    let messages = SocketService.channels[channelId].messages;
+    res.json({ messages: messages });
+  } else {
+    res.status(404).json({ error: "Channel not found" });
+  }
+});
+
+/*7. [POST] - http://adress:port/ducks/api/broadcast/
+skapar en ny nödhändelse. Detta anrop ska kräva ett giltigt JWT token.*/
+app.post("/ducks/api/broadcast/", (req, res) => {
+  const channelId = "broadcast";
+  const { username, content } = req.body; // assuming the request body contains user and content for the message
+
+  // Check if the channel exists
+  if (SocketService.channels[channelId]) {
+    // Add the new message to the channel
+    const newMessage = { username, content };
+    SocketService.channels[channelId].messages.push(newMessage);
+
+    // Emit the new message to all connected clients in the channel
+    SocketService.io.to(channelId).emit("message", newMessage);
+
+    res.sendStatus(200);
+  } else {
+    // Channel not found
+    res.status(404).json({ error: "Channel not found" });
+  }
+});
+
 
 //urls
 app.use("/ducks/api/auth", authRouter);
