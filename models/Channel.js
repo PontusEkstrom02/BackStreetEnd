578d250091717c0c3178234446906a13f8e48dd6
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-04-14 09:08:11 +0200

    Modeller för channels och Messages samt mer på Usermodellen

--- models/Channel.js
+++ models/Channel.js
@@ -1,12 +1,31 @@
 const mongoose = require("mongoose");
 
-const ChannelSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "must provide name"],
-    trim: true,
-    maxlength: [100, "A chatmessage must be shorter then 100 characters"],
+const ChannelSchema = new mongoose.Schema(
+  {
+    channelName: {
+      type: String,
+      required: [true, "must provide name for the channel"],
+      trim: true,
+      maxlength: [50, "Pls provide a shorter name for the chat"],
+    },
+    users: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    //Vi kan använda denna för att visa chanells med nyast meddelandena först/högt upp
+    latestMessage: {
+      type: mongoose.Types.ObjectId,
+      ref: "Message",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      require: [true, "pls provide user"], //Tror inte vi behöver denna rad
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Channel", ChannelSchema);
--- models/User.js
+++ models/User.js
@@ -2,28 +2,37 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs"); //bibliotek för att hasha
 const jwt = require("jsonwebtoken");
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide name"],
-    maxlength: 50,
-    minlength: 3,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide name"],
+      maxlength: 50,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide email"],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      minlength: 6,
+    },
+    picture: {
+      type: String,
+      required: true,
+      default:
+        "https://icons.iconarchive.com/icons/thesquid.ink/free-flat-sample/512/rubber-duck-icon.png",
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please provide email"],
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please provide a valid email",
-    ],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide password"],
-    minlength: 6,
-  },
-});
+  { timestamps: true }
+);
 
 UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
