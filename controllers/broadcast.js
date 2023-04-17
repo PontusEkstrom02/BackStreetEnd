23ed54be7cc1a794d00c98b4436c8019020b2076
HolaCarmensita <Casandra.c.gustafsson@gmail.com>
2023-04-17 16:31:04 +0200

    something

--- controllers/broadcast.js
+++ controllers/broadcast.js
@@ -1,31 +1,21 @@
 const SocketService = require("../service/socketService.js");
+const Channel = require("../models/Channel.js");
+//643d4fde35c69db1508a18ae
 
 /*6.[GET] - http://adress:port/ducks/api/broadcast/
 hämtar en lista över alla händelser som har skickats ut, ex. älgvandring, traffikolycker m.m.*/
 const GetBroadcast = async (req, res) => {
-
-  const channel = await Channel.findOne({
-    _id: "broadcast",
-  });
-
-  if (!channel) {
-    req.body.createdBy = "ADMIN";
-    const channel = await Channel.create(req.body);
-    res.status(StatusCodes.CREATED).json({ channel });
-  }
-
-  res.status(StatusCodes.OK).json({ channel });
+  res.json(req.body);
+  // const channel = await Channel.findOne({ _id:'643d4fde35c69db1508a18ae'
+  // });
+  // res.status(StatusCodes.OK).json({ channel });
 };
 
 /*7. [POST] - http://adress:port/ducks/api/broadcast/
 skapar en ny nödhändelse. Detta anrop ska kräva ett giltigt JWT token.*/
-const CreateBroadcastPost = async (req, res) => {
-  
-};
-
+const CreateBroadcastPost = async (req, res) => {};
 
 module.exports = {
-    GetBroadcast,
-    CreateBroadcastPost,
+  GetBroadcast,
+  CreateBroadcastPost,
 };
-  
