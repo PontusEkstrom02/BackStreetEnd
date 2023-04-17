e489717891f8cc60becacec330cc1fc689b6c115
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-04-17 09:11:33 +0200

    Update broadcast.js

--- controllers/broadcast.js
+++ controllers/broadcast.js
@@ -20,20 +20,7 @@ const GetBroadcast = async (req, res) => {
 /*7. [POST] - http://adress:port/ducks/api/broadcast/
 skapar en ny nödhändelse. Detta anrop ska kräva ett giltigt JWT token.*/
 const CreateBroadcastPost = async (req, res) => {
-  const channelId = "broadcast";
-  const { username, content } = req.body; // assuming the request body contains user and content for the message
-  // Check if the channel exists
-  if (SocketService.channels[channelId]) {
-    // Add the new message to the channel
-    const newMessage = { username, content };
-    SocketService.channels[channelId].messages.push(newMessage);
-    // Emit the new message to all connected clients in the channel
-    SocketService.io.to(channelId).emit("message", newMessage);
-    res.sendStatus(200);
-  } else {
-    // Channel not found
-    res.status(404).json({ error: "Channel not found" });
-  }
+  
 };
 
 
