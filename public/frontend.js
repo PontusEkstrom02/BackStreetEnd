6b4263aed51a57b16d5ef9a8c15a84c8c522c0d5
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-04-18 09:57:44 +0200

    delete sockets stuff

--- public/frontend.js
+++ public/frontend.js
@@ -1,14 +1,4 @@
-//const SocketService = require("./service/socketService.js");
-
 function test() {
     let response = fetch('http://127.0.0.1:3000/ducks/api/channel');
     console.log(response);
 };
-/*
-messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    SocketService.emit('send-chat-message', message)
-    messageInput.value = ''
-})
-*/
